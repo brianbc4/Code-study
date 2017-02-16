47c8e77700b099f1e38cb957727deea2e05dcac1
Brian Chen <brian.chen@crazymike.com.tw>
2017-02-16 14:07:00 +0800

    no message

---
+++ TabLayout_ViewPager/app/src/main/java/com/example/user1/tablayout_viewpager/Fragment3.java
@@ -0,0 +1,39 @@
+package com.example.user1.tablayout_viewpager;
+
+/**
+ * Created by user1 on 2017/2/2.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment3 extends Fragment {
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     @return A new instance of fragment Fragment3.
+     */
+    public static Fragment3 newInstance() {
+        return new Fragment3();
+    }
+
+    public Fragment3() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment3, container, false);
+    }
+}
---
+++ TabLayout_ViewPager/app/src/main/java/com/example/user1/tablayout_viewpager/TabsListener.java
@@ -0,0 +1,10 @@
+package com.example.user1.tablayout_viewpager;
+
+/**
+ * Created by user1 on 2017/2/2.
+ */
+
+public interface TabsListener {
+    void onTabAdded();
+    void onTabRemoved();
+}
